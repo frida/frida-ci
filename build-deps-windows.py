aa47aafe022bbe1a7f6c09af3718ff131d4e798b
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-02-23 20:40:41 +0100

    Automate Windows toolchain and sdk packaging

--- build-deps-windows.py
+++ build-deps-windows.py
@@ -1,23 +1,58 @@
+import datetime
 import glob
 import os
 import shutil
 import subprocess
 import sys
+import tempfile
 
+HSBUILD = r"C:\Program Files (x86)\HSBuild\bin\hsbuild.exe"
 DEVENV = r"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
 PYTHON2 = r"C:\Program Files\Python27\python.exe"
 GIT = r"C:\Program Files (x86)\Git\bin\git.exe"
+SZIP = r"C:\Program Files\7-Zip\7z.exe"
 
-V8_ARCH = {'x86': 'ia32', 'x86_64': 'x64'}
+V8_ARCH = {
+    "x86": "ia32",
+    "x86_64": "x64"
+}
+
+SDK_BLACKLISTED_EXTENSIONS = (
+    "exe",
+    "idb",
+    "ilk",
+    "pl",
+    "pc",
+)
+
+SDK_BLACKLISTED_FILES = (
+    "vala-0.14.lib",
+    "vala-0.14.pdb",
+    "valac-0.14.pdb",
+    "glib-genmarshal.pdb",
+    "gobject-query.pdb"
+)
 
 
 ci_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 v8_dir = os.path.join(ci_dir, "v8")
+output_dir = os.path.join(ci_dir, "__build__")
+
+def check_environment():
+    for tool in [HSBUILD, DEVENV, PYTHON2, GIT, SZIP]:
+        if not os.path.exists(tool):
+            print("ERROR: %s not found" % tool, file=sys.stderr)
+            sys.exit(1)
+
+
+#
+# Building
+#
 
 def build_hsmodules(platform, configuration):
     vala_executable = os.path.join(ci_dir, "__build__", platform, configuration, "bin", "valac-0.14.exe")
     if not os.path.exists(vala_executable):
-        perform("hsbuild", "build", "-p", platform, "-c", configuration, "-v", "glib", "libgee", "json-glib", "vala")
+        perform(HSBUILD, "build", "-p", platform, "-c", configuration, "-v", "glib", "libgee", "json-glib", "vala")
 
 def build_v8(platform, configuration, runtime):
     if not os.path.exists(v8_dir):
@@ -62,8 +97,102 @@ def v8_library(name_template, platform, configuration, runtime):
         files.append((intermediate, output))
     return files
 
+
+#
+# Packaging
+#
+
+def package():
+    now = datetime.datetime.now()
+
+    toolchain_filename = now.strftime("toolchain-windows-%Y%m%d.exe")
+    toolchain_path = os.path.join(ci_dir, toolchain_filename)
+
+    sdk_filename = now.strftime("sdk-windows-%Y%m%d.exe")
+    sdk_path = os.path.join(ci_dir, sdk_filename)
+
+    if os.path.exists(toolchain_path) and os.path.exists(sdk_path):
+        return
+
+    print("About to assemble:")
+    print("\t* " + toolchain_filename)
+    print("\t* " + sdk_filename)
+    print()
+    print("Determining what to include...")
+
+    sdk_built_files = []
+    for root, dirs, files in os.walk(output_dir):
+        relpath = root[len(output_dir) + 1:]
+        included_files = map(lambda name: os.path.join(relpath, name),
+            filter(lambda filename: file_is_sdk_related(root, filename), files))
+        sdk_built_files.extend(included_files)
+
+    toolchain_files = []
+    for root, dirs, files in os.walk(os.path.join(output_dir, 'x86', 'Release')):
+        relpath = root[len(output_dir) + 1:]
+        included_files = map(lambda name: os.path.join(relpath, name),
+            filter(lambda filename: file_is_vala_toolchain_related(root, filename), files))
+        toolchain_files.extend(included_files)
+
+    sdk_built_files.sort()
+    toolchain_files.sort()
+
+    print("Copying files...")
+    tempdir = tempfile.mkdtemp(prefix="frida-package")
+    copy_files(output_dir, sdk_built_files, os.path.join(tempdir, "sdk-windows"), transform_sdk_dest)
+    copy_files(output_dir, toolchain_files, os.path.join(tempdir, "toolchain-windows"), transform_toolchain_dest)
+
+    print("Compressing...")
+    prevdir = os.getcwd()
+    os.chdir(tempdir)
+
+    perform(SZIP, "a", "-mx", "-sfx7zCon.sfx", "-r", toolchain_path, "toolchain-windows")
+
+    perform(SZIP, "a", "-mx", "-sfx7zCon.sfx", "-r", sdk_path, "sdk-windows")
+
+    os.chdir(prevdir)
+    shutil.rmtree(tempdir)
+
+    print("All done.")
+
+def file_is_sdk_related(directory, filename):
+    base, ext = os.path.splitext(filename)
+    ext = ext[1:]
+    if ext in SDK_BLACKLISTED_EXTENSIONS:
+        return False
+    elif ext == "h" and base.startswith("vala"):
+        return False
+    elif ext in ("vapi", "deps"):
+        if base == "libvala-0.14":
+            return False
+        return not directory.endswith("share\\vala-0.14\\vapi")
+    return filename not in SDK_BLACKLISTED_FILES
+
+def file_is_vala_toolchain_related(directory, filename):
+    base, ext = os.path.splitext(filename)
+    ext = ext[1:]
+    if ext in ('vapi', 'deps'):
+        return directory.endswith("share\\vala-0.14\\vapi")
+    return filename == "valac-0.14.exe"
+
+def transform_identity(srcfile):
+    return srcfile
+
+def transform_sdk_dest(srcfile):
+    dstfile = srcfile.replace("x86_64\\", "x64-")
+    dstfile = dstfile.replace("x86\\", "Win32-")
+    return dstfile
+
+def transform_toolchain_dest(srcfile):
+    return srcfile[srcfile.index("\\Release\\") + 9:]
+
+
+#
+# Utilities
+#
+
 def perform(*args, **kwargs):
-    print " ".join(args)
+    print(" ".join(args))
     subprocess.check_call(args, **kwargs)
 
 def copy(src, dst):
@@ -72,10 +201,21 @@ def copy(src, dst):
         os.makedirs(dst_dir)
     shutil.copyfile(src, dst)
 
+def copy_files(fromdir, files, todir, transformdest=transform_identity):
+    for file in files:
+        src = os.path.join(fromdir, file)
+        dst = os.path.join(todir, transformdest(file))
+        dstdir = os.path.dirname(dst)
+        if not os.path.isdir(dstdir):
+            os.makedirs(dstdir)
+        shutil.copyfile(src, dst)
+
 
 if __name__ == '__main__':
-    for platform in ['x86_64', 'x86']:
-        for configuration in ['Debug', 'Release']:
+    check_environment()
+    for platform in ["x86_64", "x86"]:
+        for configuration in ["Debug", "Release"]:
             build_hsmodules(platform, configuration)
             for runtime in ['static', 'dynamic']:
                 build_v8(platform, configuration, runtime)
+    package()
