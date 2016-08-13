370199d4f9c47d664307565a7a6d4b0f6b12929f
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-08-13 18:56:08 +0200

    Update to the new canonical Python 2.7 location

--- build-deps-windows.py
+++ build-deps-windows.py
@@ -9,7 +9,7 @@ import tempfile
 
 HSBUILD = r"C:\Program Files (x86)\HSBuild\bin\hsbuild.exe"
 DEVENV = r"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.com"
-PYTHON2 = r"C:\Program Files\Python27\python.exe"
+PYTHON2 = r"C:\Program Files\Python 2.7\python.exe"
 GIT = r"C:\Program Files\Git\bin\git.exe"
 SZIP = r"C:\Program Files\7-Zip\7z.exe"
 
